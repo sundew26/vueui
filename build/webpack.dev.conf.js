2ff35185c65a1d31e4cc7866d78b241bada7ab76
魏露 <hdpu_weilu_0791@163.com>
2017-03-01 15:25:23 +0800

    vue组件库环境搭建成功

---
+++ build/webpack.dev.conf.js
@@ -0,0 +1,57 @@
+var utils = require('./utils')
+var webpack = require('webpack')
+var config = require('../config')
+var merge = require('webpack-merge')
+var baseWebpackConfig = require('./webpack.base.conf')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+var entry = require('./getentry')
+
+// add hot-reload related code to entry chunks
+Object.keys(baseWebpackConfig.entry).forEach(function (name) {
+  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
+})
+
+var webpackConfig = merge(baseWebpackConfig, {
+  module: {
+    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
+  },
+  // cheap-module-eval-source-map is faster for development
+  devtool: '#cheap-module-eval-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': config.dev.env
+    }),
+    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    // https://github.com/ampedandwired/html-webpack-plugin
+    new FriendlyErrorsPlugin()
+  ]
+})
+
+var pages = Object.keys(entry.getEntry('src/demo/*.html', 'src/demo/'));
+pages.forEach(function(pathname) {
+  var conf = {
+    filename: config.build.assetsRoot + '/demo/' + pathname + '.html',
+    template: 'src/demo/' + pathname + '.html',
+    inject: true,
+    minify: {
+      removeComments: true,
+      collapseWhitespace: true,
+      removeAttributeQuotes: true
+      // more options:
+      // https://github.com/kangax/html-minifier#options-quick-reference
+    },
+    // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+    chunksSortMode: 'dependency'
+  };
+  if (pathname in baseWebpackConfig.entry) {
+    conf.inject = 'body';
+    conf.chunks = ['vendors', pathname];
+    conf.hash = true;
+  }
+  webpackConfig.plugins.push(new HtmlWebpackPlugin(conf));
+})
+
+module.exports = webpackConfig
