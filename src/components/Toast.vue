6899cee8f0c69e566e097f5e321fe143495c077c
魏露 <hdpu_weilu_0791@163.com>
2017-03-07 17:19:20 +0800

    toast插件完成  位置有: 顶部  中间  底部   类型有 text warn cancel

---
+++ src/components/Toast.vue
@@ -0,0 +1,94 @@
+<template>
+  <transition name="toast">
+    <div class="toast" v-show="options.show==true"
+         :class="{top: options.position==='top', middle: options.position==='middle', bottom: options.position==='bottom'}">
+      <span v-if="options.type==='cancel'" class="icon-cancel"></span>
+      <span v-if="options.type==='warn'" class="icon-warn"></span>
+      {{options.msg}}
+    </div>
+  </transition>
+</template>
+<script>
+  export default {
+    props: {
+      toast: {
+        type: Object,
+        default: function () {
+          return {
+            show: false, // 显示toast
+            position: 'middle', // toast位置 top middle bottom
+            type: 'text', // 类型 text cancel warn
+            time: '3000' // 显示时间 默认3s
+          }
+        }
+      }
+    },
+    data () {
+      return {
+        options: this.toast
+      }
+    },
+    methods: {
+      autoHide (time) {
+        setTimeout(function () {
+          this.options.show = false
+        }.bind(this), time)
+      }
+    },
+    mounted () {
+      this.autoHide(this.options.time)
+    },
+    watch: {
+      toast: {
+        handler: function (option) {
+          console.log(1, option.time)
+          this.options = option
+          this.autoHide(option.time)
+        },
+        deep: true
+      }
+    }
+  }
+</script>
+<style scoped>
+  @import "../static/iconfont.scss";
+  .toast {
+    padding: 0 10px;
+    max-width: 80%;
+    line-height: 30px;
+    height: 30px;
+    background: rgba(0, 0, 0, 0.7);
+    border-radius: 4px;
+    text-align: center;
+    position: fixed;
+    color: #fff;
+    font-size: 12px;
+    box-sizing: border-box;
+    z-index: 99999;
+    transition: all ease .3s;
+  }
+  .top {
+    top: 100px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  .middle {
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%);
+  }
+  .bottom {
+    bottom: 100px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  /*vue2.0 transition*/
+  .toast-enter-active, .toast-leave-active {
+    transition: opacity .6s;
+    opacity: 1;
+  }
+  .toast-enter, .toast-leave-to{
+    opacity: 0;
+  }
+</style>
---
+++ src/pages/Toast.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="content">
+    <input type="button" @click="showToast(1)" value="默认"/>
+    <input type="button" @click="showToast(2)" value="中部"/>
+    <input type="button" @click="showToast(3)" value="顶部"/>
+    <input type="button" @click="showToast(4)" value="底部"/>
+    <input type="button" @click="showToast(5)" value="警告"/>
+    <input type="button" @click="showToast(6)" value="取消"/>
+    <input type="button" @click="showToast(7)" value="1s消失"/>
+    <Toast :toast="toast"></Toast>
+  </div>
+</template>
+
+<script>
+  import Toast from '../components/Toast.vue'
+  export default {
+    components: {
+      Toast
+    },
+    data () {
+      return {
+        toast: {
+          show: true,
+          position: 'top',
+          type: 'text',
+          time: '2000',
+          msg: 'hello vueUI'
+        }
+      }
+    },
+    methods: {
+      showToast (item) {
+        switch (item) {
+          case 1:
+            this.toast = {
+              show: true,
+              position: 'middle',
+              type: 'text',
+              time: '3000',
+              msg: 'hello vueUI'
+            }
+            break
+          case 2:
+            this.toast = {
+              show: true,
+              position: 'middle',
+              type: 'text',
+              time: '3000',
+              msg: 'hello vueUI'
+            }
+            break
+          case 3:
+            this.toast = {
+              show: true,
+              position: 'top',
+              type: 'text',
+              time: '3000',
+              msg: 'hello vueUI'
+            }
+            break
+          case 4:
+            this.toast = {
+              show: true,
+              position: 'bottom',
+              type: 'text',
+              time: '3000',
+              msg: 'hello vueUI'
+            }
+            break
+          case 5:
+            this.toast = {
+              show: true,
+              position: 'middle',
+              type: 'warn',
+              time: '3000',
+              msg: 'hello vueUI'
+            }
+            break
+          case 6:
+            this.toast = {
+              show: true,
+              position: 'middle',
+              type: 'cancel',
+              time: '6000',
+              msg: 'hello vueUI'
+            }
+            break
+          case 7:
+            this.toast = {
+              show: true,
+              position: 'middle',
+              type: 'cancel',
+              time: '1000',
+              msg: 'hello vueUI'
+            }
+            break
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
