d17485c79c0f99c6c05289179e38aee6834ccc99
stardew <wl@kaishiba.com>
2018-08-09 21:58:52 +0800

    dist/components

---
+++ src/components/Panel.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="panel">
+    <slot name="title"></slot>
+    <div class="info" :style="{'border-width': border}">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      border: {
+        type: [Number, String],
+        default: 0
+      }
+    },
+    data () {
+      return {
+      }
+    },
+    watch: {
+    },
+    methods: {
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  @import "../static/iconfont.scss";
+  .panel {
+    width: 100%;
+    box-sizing: border-box;
+    text-align: left;
+    margin-top: 10rpx;
+    .title {
+      padding: 20rpx;
+      font-size: 12px;
+    }
+    .info {
+      border-style: solid;
+      border-color: #e1e1e1;
+      border-width: 0;
+      font-size: 12px;
+      background-color: #fff;
+      display: block;
+      padding: 10px;
+    }
+  }
+</style>
